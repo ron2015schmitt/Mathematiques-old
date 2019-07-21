9804410cbe7ae4d52d00e19fc9f32af4d8aafe6d
Ronald Schmitt <Ron.Schmitt@caa.columbia.edu>
2019-07-21 14:46:04 -0400

    initial upload

---
+++ development/Complex.hpp
@@ -0,0 +1,36 @@
+
+
+#include <complex>
+
+namespace COOLL {
+
+  template <typename D> class Complex : complex<D> {
+
+  public:
+    
+
+
+  };
+
+
+
+  template <typename D> class Imaginary  {
+
+  public:
+    
+
+
+  };
+
+
+  template <typename D> class UnitImaginary : Imaginary<D> {
+
+  public:
+    
+
+
+  };
+
+
+
+};

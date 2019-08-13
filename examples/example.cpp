1f39555d70d9e764720572a91f394f08c72a0983
ron2015schmitt <Ron.Schmitt@caa.columbia.edu>
2019-08-13 17:06:07 -0400

    adding example source file

---
+++ examples/example.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+#include <string>
+
+#include "matricks.hpp"
+
+
+int main()
+{
+
+  using namespace Matricks;
+  Vector<double> v1(2, 3.14);
+
+  std::cout << std::endl << v1 << std::endl;
+  
+  return 0;
+}

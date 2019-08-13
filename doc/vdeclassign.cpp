96bc675a51798595129fa526711a994cd9785578
ron2015schmitt <Ron.Schmitt@caa.columbia.edu>
2019-08-13 10:13:08 -0400

    updated doc

--- doc/vdeclassign.cpp
+++ doc/vdeclassign.cpp
@@ -12,6 +12,8 @@ using namespace Matricks;
 // used to demonstrate how to convert to C++ vectors
 #include <vector>
 
+char _str[2048];
+
 
 #define header1(x)  printf("# %s \n", x)
 #define header2(x)  printf("## %s \n", x)
@@ -20,7 +22,8 @@ using namespace Matricks;
 
 #define printexp(x)  printf("-> %s = %s\n",  #x, x)
 
-#define codestart() printf("```C++\n")
+#define codestart(lang) std::cout << std::string("```") << std::string(lang) << std::string("\n")
+//#define codestart() printf( "```\n" )
 #define codemulti(...)  printf("%s;\n",  #__VA_ARGS__); __VA_ARGS__
 
 #define codeend() printf("```\n")
@@ -32,8 +35,8 @@ using namespace Matricks;
 #define result(x)  resultstart();resultmulti(x);resultend()
 
 int _e = 1;
-char _str[2048];
 
+//#define example(x) sprintf(_str,"**EXAMPLE%2d**: ",_e++); text(std::strcat(_str,x))
 #define example(x) sprintf(_str,"**EXAMPLE%2d**: ",_e++); text(std::strcat(_str,x))
 
 
@@ -46,27 +49,31 @@ char _str[2048];
 
 int main()
 {
+
+
+  
   cr();
   header1("Declaring and Assigning Vectors in mātricks");
-  cr();
+  printf("This document is automatically generated from file **`%s`**:\n",__FILE__);
+  
   header2("Declaring vectors");
   
   example("Declare vector `v1`.");
-  codestart();
-  codemulti( const int N = 4 );
+  codestart("C++");
+  codemulti( const size_type N = 4 );
   codemulti( Vector<double> v1(N) );
   codeend();
   result(v1);
 
 
   example("Declare `v2` and initialize to a constant" );
-  codestart();
+  codestart("C++");
   codemulti( Vector<double> v2(N, -1 ) );
   codeend();
   result(v2);
 
   example("Declare `v3` and initialize to a **C-style array** of values" );
-  codestart();
+  codestart("C++");
   codemulti( Vector<double> v3( N, (const double[4]) {10, 20, 30, 40} )  );
   codeend();
   result(v3);
@@ -79,7 +86,7 @@ int main()
   cr();
   cr();
   example("Set each element of `v1` **individually**");
-  codestart();
+  codestart("C++");
   codemulti( v1[0] = 1.0 );
   codemulti( v1[1] = 2.0 );
   codemulti( v1[2] = 3.0 );
@@ -90,7 +97,7 @@ int main()
 
 
   example("Assigning `v` from a **C-style array**" );
-  codestart();
+  codestart("C++");
   codemulti( v2 = Vector<double>( N, (const double[]) {10, 20, 30, 40})  );
   codeend();
   result(v2);
@@ -135,211 +142,211 @@ int main()
 
 
 
-  std::cout << std::endl << "initialize v2 from std::vector" << std::endl;
-  std::vector<double> vstd(4);
-  vstd[0] = -1;
-  vstd[1] = -2;
-  vstd[2] = -3;
-  vstd[3] = -4;
-  v2 = vcast<double>(vstd);
-  cr();
-  result(v2);
+  // std::cout << std::endl << "initialize v2 from std::vector" << std::endl;
+  // std::vector<double> vstd(4);
+  // vstd[0] = -1;
+  // vstd[1] = -2;
+  // vstd[2] = -3;
+  // vstd[3] = -4;
+  // v2 = vcast<double>(vstd);
+  // cr();
+  // result(v2);
 
   
-  std::cout << std::endl << "initialize v2 from std::valarray" << std::endl;
-  std::valarray<double> val(4);
-  val[0] = -11;
-  val[1] = -12;
-  val[2] = -13;
-  val[3] = -14;
-  v2 = vcast<double>(val);
-  dispcr(v2);
+  // std::cout << std::endl << "initialize v2 from std::valarray" << std::endl;
+  // std::valarray<double> val(4);
+  // val[0] = -11;
+  // val[1] = -12;
+  // val[2] = -13;
+  // val[3] = -14;
+  // v2 = vcast<double>(val);
+  // dispcr(v2);
 
-  std::cout << std::endl << "initialize v2 using range<" << std::endl;
-  v2 = range<double>(100,400,100);
-  dispcr(v2);
+  // std::cout << std::endl << "initialize v2 using range<" << std::endl;
+  // v2 = range<double>(100,400,100);
+  // dispcr(v2);
 
-  std::cout << std::endl << "addition of vectors" << std::endl;
-  v3 = v1 + v2;
-  std::cout << " v3 = v1 + v2 = " <<v3<<std::endl;
+  // std::cout << std::endl << "addition of vectors" << std::endl;
+  // v3 = v1 + v2;
+  // std::cout << " v3 = v1 + v2 = " <<v3<<std::endl;
 
-  std::cout << std::endl << "vector subtraction" << std::endl;
-  v3 = v2 - v1;
-  std::cout << " v3 = v2 - v1 = " <<v3<<std::endl;
+  // std::cout << std::endl << "vector subtraction" << std::endl;
+  // v3 = v2 - v1;
+  // std::cout << " v3 = v2 - v1 = " <<v3<<std::endl;
 
-  std::cout << std::endl << "vector (element-wise) multiplication" << std::endl;
-  v3 = v1 * v2;
-  std::cout << " v3 = v1 * v2 = " <<v3<<std::endl;
+  // std::cout << std::endl << "vector (element-wise) multiplication" << std::endl;
+  // v3 = v1 * v2;
+  // std::cout << " v3 = v1 * v2 = " <<v3<<std::endl;
 
-  std::cout << std::endl << "vector (element-wise) division" << std::endl;
-  v3 =  v2/v1;
-  std::cout << " v3 = v2 / v1 = " <<v3<<std::endl;
-
-
-
-  std::cout << std::endl << "vector + scalar" << std::endl;
-  v3 =  v2/v1;
-  std::cout << " v3 = v2 / v1 = " <<v3<<std::endl;
+  // std::cout << std::endl << "vector (element-wise) division" << std::endl;
+  // v3 =  v2/v1;
+  // std::cout << " v3 = v2 / v1 = " <<v3<<std::endl;
 
 
-  std::cout << std::endl << "complicated example#1" << std::endl;
-  v3 = 3*(v1+v2) + 1000;
-  std::cout << " v3 =  3*(v1+v2) + 1000 = " <<v3<<std::endl;
+
+  // std::cout << std::endl << "vector + scalar" << std::endl;
+  // v3 =  v2/v1;
+  // std::cout << " v3 = v2 / v1 = " <<v3<<std::endl;
 
 
-  std::cout << std::endl << "complicated example#2" << std::endl;
-  v3 = -200 + 100*v1;
-  std::cout << " v3 =  -200 + 100*v1 = " <<v3<<std::endl;
+  // std::cout << std::endl << "complicated example#1" << std::endl;
+  // v3 = 3*(v1+v2) + 1000;
+  // std::cout << " v3 =  3*(v1+v2) + 1000 = " <<v3<<std::endl;
 
 
-  std::cout << std::endl << "intitialize elements of v2 to the same value" << std::endl;
-  v2 = 1;
-  std::cout <<"v2 = 1 = "<< v2<< std::endl;
+  // std::cout << std::endl << "complicated example#2" << std::endl;
+  // v3 = -200 + 100*v1;
+  // std::cout << " v3 =  -200 + 100*v1 = " <<v3<<std::endl;
+
+
+  // std::cout << std::endl << "intitialize elements of v2 to the same value" << std::endl;
+  // v2 = 1;
+  // std::cout <<"v2 = 1 = "<< v2<< std::endl;
   
 
-  std::cout << "dot product" << std::endl;
-  double d = (v1|v2);
-  std::cout <<d << std::endl;
+  // std::cout << "dot product" << std::endl;
+  // double d = (v1|v2);
+  // std::cout <<d << std::endl;
 
-  std::cout << "dot product" << std::endl;
-  d = (v1|(v1+3*v2)) + 1.0;
-  std::cout <<d << std::endl;
+  // std::cout << "dot product" << std::endl;
+  // d = (v1|(v1+3*v2)) + 1.0;
+  // std::cout <<d << std::endl;
 
-  std::cout << "maximum" << std::endl;
-  d = max(v1+v2);
-  std::cout <<d << std::endl;
+  // std::cout << "maximum" << std::endl;
+  // d = max(v1+v2);
+  // std::cout <<d << std::endl;
 
-  std::cout << "minimum" << std::endl;
-  d = min(v3);
-  std::cout <<d << std::endl;
+  // std::cout << "minimum" << std::endl;
+  // d = min(v3);
+  // std::cout <<d << std::endl;
 
-  std::cout << "sum" << std::endl;
-  d = sum(v1);
-  std::cout <<d << std::endl;
+  // std::cout << "sum" << std::endl;
+  // d = sum(v1);
+  // std::cout <<d << std::endl;
 
-  std::cout << "norm" << std::endl;
-  d = norm(v1);
-  std::cout <<d << std::endl;
+  // std::cout << "norm" << std::endl;
+  // d = norm(v1);
+  // std::cout <<d << std::endl;
 
-  Vector<int> v4(N,"v4");
-  v4 = range<int>(20,17);   
-  std::cout << "vector of integers" << std::endl;
-  std::cout <<v4 << std::endl;
-
-
-  Vector<unsigned int> v4u(N,"v4u");
-  v4u = range<uint>(5,8);
-  std::cout << "vector of unsigned integers" << std::endl;
-  std::cout <<v4u << std::endl;
-
-  Vector<float> v5(N,"v5");
-  std::cout << "converting integer vector to single precision vector and adding a constant" << std::endl;
-  v5 = vcast<float>(v4) + 0.5F;
-  std::cout << v5 << std::endl;
+  // Vector<int> v10(N,"v10");
+  // v10 = range<int>(20,17);   
+  // std::cout << "vector of integers" << std::endl;
+  // std::cout <<v4 << std::endl;
 
 
-  Vector<extended> v6(N,"v6");
-  std::cout << "converting double precision vector to extended precision vector and addign a constant" << std::endl;
-  v6 = vcast<extended>(v1) + 1e-18L;
-  std::cout.precision(19);
-  std::cout << v6 << std::endl;
+//   Vector<unsigned int> v10u(N,"v10u");
+//   v10u = range<uint>(5,8);
+//   std::cout << "vector of unsigned integers" << std::endl;
+//   std::cout <<v4u << std::endl;
 
-  std::cout.precision(0);
-
-
-
-  std::cout<<std::endl << "resize v1: v1.resize(3). Note that all data that as in v1 is lost." << std::endl;
-  v1.resize(3);
-  std::cout <<"v1 = "<< v1<< std::endl;
+//   Vector<float> v5(N,"v5");
+//   std::cout << "converting integer vector to single precision vector and adding a constant" << std::endl;
+//   v5 = vcast<float>(v4) + 0.5F;
+//   std::cout << v5 << std::endl;
 
 
-  std::cout<<std::endl << "resize to zero to release the memory: v1.resize(0)" << std::endl;
-  std::cout << "this can be used for efficient memory management when working with large vectors" << std::endl;
-  v1.resize(0);
-  std::cout <<"v1 = "<< v1<< std::endl;
+//   Vector<extended> v6(N,"v6");
+//   std::cout << "converting double precision vector to extended precision vector and addign a constant" << std::endl;
+//   v6 = vcast<extended>(v1) + 1e-18L;
+//   std::cout.precision(19);
+//   std::cout << v6 << std::endl;
 
-  std::cout<<std::endl << "resize and set v1 froma vector expression" << std::endl;
-  v1.resize() = 2*v2;
-  std::cout <<"v1.resize() = 2*v2 = "<< v1<< std::endl;
-
-
-  std::cout<<std::endl << "reinitialize v1" << std::endl;
-  v1[0] = -1.5;
-  v1[1] = -1.1;
-  v1[2] = 0.1;
-  v1[3] = 0.5;
-  std::cout << v1 << std::endl;
+//   std::cout.precision(0);
 
 
-  std::cout <<std::endl<< "convert v1 to a C-style array of floats" << std::endl;
-  float* dat1 = toCarray<float>(v1);
-  std::cout << "*dat1={";
-  for(unsigned int i = 0; i<N-1; i++) 
-    std::cout <<dat1[i]<<",";
-  std::cout <<dat1[N-1] << "}"<<std::endl;
-  // YOU MUST DELETE THE MEMORY THAT WAS CREATED AT SOME POINT BEFORE 
-  // THE END OF YOUR CODE 
-  delete [] dat1;
+
+//   std::cout<<std::endl << "resize v1: v1.resize(3). Note that all data that as in v1 is lost." << std::endl;
+//   v1.resize(3);
+//   std::cout <<"v1 = "<< v1<< std::endl;
 
 
-  std::cout <<std::endl<< "round and convert v1 to a std::vector of ints" << std::endl;
-  std::vector<int> vstd2 = tostdvector<int>(round(v1));
-  std::cout << "vstd2={";
-  for(unsigned int i = 0; i<N-1; i++) 
-    std::cout <<vstd2[i]<<",";
-  std::cout <<vstd2[N-1] << "}"<<std::endl;
+//   std::cout<<std::endl << "resize to zero to release the memory: v1.resize(0)" << std::endl;
+//   std::cout << "this can be used for efficient memory management when working with large vectors" << std::endl;
+//   v1.resize(0);
+//   std::cout <<"v1 = "<< v1<< std::endl;
+
+//   std::cout<<std::endl << "resize and set v1 froma vector expression" << std::endl;
+//   v1.resize() = 2*v2;
+//   std::cout <<"v1.resize() = 2*v2 = "<< v1<< std::endl;
 
 
-  std::cout <<std::endl<< "convert v1 to a std::valarray of doubles" << std::endl;
-  std::valarray<double> val2 = tovalarray<double>(v1);
-  std::cout << "val2={";
-  for(unsigned int i = 0; i<N-1; i++) 
-    std::cout <<val2[i]<<",";
-  std::cout <<val2[N-1] << "}"<<std::endl;
+//   std::cout<<std::endl << "reinitialize v1" << std::endl;
+//   v1[0] = -1.5;
+//   v1[1] = -1.1;
+//   v1[2] = 0.1;
+//   v1[3] = 0.5;
+//   std::cout << v1 << std::endl;
+
+
+//   std::cout <<std::endl<< "convert v1 to a C-style array of floats" << std::endl;
+//   float* dat1 = toCarray<float>(v1);
+//   std::cout << "*dat1={";
+//   for(unsigned int i = 0; i<N-1; i++) 
+//     std::cout <<dat1[i]<<",";
+//   std::cout <<dat1[N-1] << "}"<<std::endl;
+//   // YOU MUST DELETE THE MEMORY THAT WAS CREATED AT SOME POINT BEFORE 
+//   // THE END OF YOUR CODE 
+//   delete [] dat1;
+
+
+//   std::cout <<std::endl<< "round and convert v1 to a std::vector of ints" << std::endl;
+//   std::vector<int> vstd2 = tostdvector<int>(round(v1));
+//   std::cout << "vstd2={";
+//   for(unsigned int i = 0; i<N-1; i++) 
+//     std::cout <<vstd2[i]<<",";
+//   std::cout <<vstd2[N-1] << "}"<<std::endl;
+
+
+//   std::cout <<std::endl<< "convert v1 to a std::valarray of doubles" << std::endl;
+//   std::valarray<double> val2 = tovalarray<double>(v1);
+//   std::cout << "val2={";
+//   for(unsigned int i = 0; i<N-1; i++) 
+//     std::cout <<val2[i]<<",";
+//   std::cout <<val2[N-1] << "}"<<std::endl;
 
     
 
 
-  std::cout <<std::endl<< "accessing a range of elements using the seq(start,end)" << std::endl;
-  Vector<double> v7(0,"v7");
-  v7.resize() = v1[seq(1,3)];
-  std::cout <<"v7.resize() = v1[seq(1,3)]=" << v7<<std::endl;
+//   std::cout <<std::endl<< "accessing a range of elements using the seq(start,end)" << std::endl;
+//   Vector<double> v7(0,"v7");
+//   v7.resize() = v1[seq(1,3)];
+//   std::cout <<"v7.resize() = v1[seq(1,3)]=" << v7<<std::endl;
 
-  std::cout <<std::endl<< "zero teh first two elements of v7" << std::endl;
-  v7[seq(0,1)]=0;
-  std::cout <<"v7[seq(0,1)]=0; v7=" << v7<<std::endl;
-
-
-  std::cout <<std::endl<< "reverse the order of a vector" << std::endl;
-  v1 = v1[seq(3,0)];
-  std::cout <<"v1 = v1[seq(3,0)]=" << v1<<std::endl;
-
-    text("The optional second parameter is the name that will be stored with the vector in the object pool.  This name can be any string that you like.");
+//   std::cout <<std::endl<< "zero teh first two elements of v7" << std::endl;
+//   v7[seq(0,1)]=0;
+//   std::cout <<"v7[seq(0,1)]=0; v7=" << v7<<std::endl;
 
 
-      example("Assigning `v1` from a **C-style array**" );
-  codestart();
-  codemulti( double temp[N] = {10, 20, 30, 40} );
-  codemulti( v1 = vcast<double>(temp,N) );
-  codeend();
-  result(v1);
-text("There's the macro **`Vector_`** name the vector automatically\n");
-  codestart();
-  codemulti( Vector_(double, y, N) );
-  codeend();
-  result(y);
+//   std::cout <<std::endl<< "reverse the order of a vector" << std::endl;
+//   v1 = v1[seq(3,0)];
+//   std::cout <<"v1 = v1[seq(3,0)]=" << v1<<std::endl;
 
-  text("There's the macro **`Vector2_`** name the vector automatically\n");
-  codestart();
-  codemulti( RVector2_(double, z, N) );
-  codeend();
-  result(z);
+//     text("The optional second parameter is the name that will be stored with the vector in the object pool.  This name can be any string that you like.");
 
-    text("When the name is not specified, the pool manager assigns one.");
-  codestart();
-  codemulti( Vector<double> x(N) );
-  codeend();
-  result(x);
+
+//       example("Assigning `v1` from a **C-style array**" );
+//   codestart("C++");
+//   codemulti( double temp[N] = {10, 20, 30, 40} );
+//   codemulti( v1 = vcast<double>(temp,N) );
+//   codeend();
+//   result(v1);
+// text("There's the macro **`Vector_`** name the vector automatically\n");
+//   codestart("C++");
+//   codemulti( Vector_(double, y, N) );
+//   codeend();
+//   result(y);
+
+//   text("There's the macro **`Vector2_`** name the vector automatically\n");
+//   codestart("C++");
+//   codemulti( RVector2_(double, z, N) );
+//   codeend();
+//   result(z);
+
+//     text("When the name is not specified, the pool manager assigns one.");
+//   codestart("C++");
+//   codemulti( Vector<double> x(N) );
+//   codeend();
+//   result(x);
 
   return 0;
 }
--- doc/vdeclassign.md
+++ doc/vdeclassign.md
@@ -1,10 +1,10 @@
 
 # Declaring and Assigning Vectors in mātricks 
-
+This document is automatically generated from file **`vdeclassign.cpp`**:
 ## Declaring vectors 
 **EXAMPLE 1**: Declare vector `v1`. 
 ```C++
-const int N = 4;
+const size_type N = 4;
 Vector<double> v1(N);
 ```
 **The result is**
@@ -62,7 +62,7 @@ HELLO
 
 
 **EXAMPLE 6**: Assign `v1` using the **`range`** function 
-```C++
+```
 v1 = range<double>(5,8);
 ```
 
@@ -74,7 +74,7 @@ v1 = range<double>(5,8);
 
 
 **EXAMPLE 7**: Assign `v1` using the **`range`** function 
-```C++
+```
 v1 = range<double>(4,1);
 ```
 
@@ -86,7 +86,7 @@ v1 = range<double>(4,1);
 
 
 **EXAMPLE 8**: Assign `v1` using the **`range`** function 
-```C++
+```
 v1 = range<double>(400,100,-100);
 ```
 
@@ -99,7 +99,7 @@ v1 = range<double>(400,100,-100);
 
 
 **EXAMPLE 9**: Assign `v1` using the **`linspace`** function 
-```C++
+```
 v1 = linspace<double>(100,400,4);
 ```
 
@@ -111,141 +111,12 @@ v1 = linspace<double>(100,400,4);
 
 
 **EXAMPLE10**: Assign `v1` using the **`linspace`** function 
-```C++
+```
 v1 = linspace<double>(6,3,4);
 ```
 
 **The result is**
 ```C++
   v1: {6,5,4,3}; 
-```
-
-
-initialize v2 from std::vector
-
-**The result is**
-```C++
-  v2: {-1,-2,-3,-4}; 
-```
-
-
-initialize v2 from std::valarray
-v2: {-11,-12,-13,-14}; 
-
-initialize v2 using range<
-v2: {100,200,300,400}; 
-
-addition of vectors
- v3 = v1 + v2 = {106,205,304,403}
-
-vector subtraction
- v3 = v2 - v1 = {94,195,296,397}
-
-vector (element-wise) multiplication
- v3 = v1 * v2 = {600,1000,1200,1200}
-
-vector (element-wise) division
- v3 = v2 / v1 = {16.6667,40,75,133.333}
-
-vector + scalar
- v3 = v2 / v1 = {16.6667,40,75,133.333}
-
-complicated example#1
- v3 =  3*(v1+v2) + 1000 = {1318,1615,1912,2209}
-
-complicated example#2
- v3 =  -200 + 100*v1 = {400,300,200,100}
-
-intitialize elements of v2 to the same value
-v2 = 1 = {1,1,1,1}
-dot product
-18
-dot product
-141
-maximum
-7
-minimum
-100
-sum
-18
-norm
-9.27362
-vector of integers
-{20,19,18,17}
-vector of unsigned integers
-{5,6,7,8}
-converting integer vector to single precision vector and adding a constant
-{20.5,19.5,18.5,17.5}
-converting double precision vector to extended precision vector and addign a constant
-{6.000000000000000001,5.000000000000000001,4.000000000000000001,3.000000000000000001}
-
-resize v1: v1.resize(3). Note that all data that as in v1 is lost.
-v1 = {0,0,0}
-
-resize to zero to release the memory: v1.resize(0)
-this can be used for efficient memory management when working with large vectors
-v1 = {}
-
-resize and set v1 froma vector expression
-v1.resize() = 2*v2 = {2,2,2,2}
-
-reinitialize v1
-{-2,-1,0.1,0.5}
-
-convert v1 to a C-style array of floats
-*dat1={-2,-1,0.1,0.5}
-
-round and convert v1 to a std::vector of ints
-vstd2={-2,-1,0,1}
-
-convert v1 to a std::valarray of doubles
-val2={-2,-1,0.1,0.5}
-
-accessing a range of elements using the seq(start,end)
-v7.resize() = v1[seq(1,3)]={-1,0.1,0.5}
-
-zero teh first two elements of v7
-v7[seq(0,1)]=0; v7={0,0,0.5}
-
-reverse the order of a vector
-v1 = v1[seq(3,0)]={0.5,0.1,-1,-2}
-The optional second parameter is the name that will be stored with the vector in the object pool.  This name can be any string that you like. 
-**EXAMPLE11**: Assigning `v1` from a **C-style array** 
-```C++
-double temp[N] = {10, 20, 30, 40};
-v1 = vcast<double>(temp,N);
-```
-**The result is**
-```C++
-  v1: {1e+01,2e+01,3e+01,4e+01}; 
-```
-
-There's the macro **`Vector_`** name the vector automatically
- 
-```C++
-Vector_(double, y, N);
-```
-**The result is**
-```C++
-  y: {0,0,0,0}; 
-```
-
-There's the macro **`Vector2_`** name the vector automatically
- 
-```C++
-RVector2_(double, z, N);
-```
-**The result is**
-```C++
-  z: {0,0,0,0}; 
-```
-
-When the name is not specified, the pool manager assigns one. 
-```C++
-Vector<double> x(N);
-```
-**The result is**
-```C++
-  x: {0,0,0,0}; 
 ```
 

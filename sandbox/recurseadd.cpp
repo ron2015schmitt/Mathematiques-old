2b261740553e3fbc574842fc758bcdc4019a615c
ron2015schmitt <Ron.Schmitt@caa.columbia.edu>
2019-11-28 18:51:19 -0500

    tests with fixed vectors

--- sandbox/recurseadd.cpp
+++ sandbox/recurseadd.cpp
@@ -140,7 +140,7 @@ int main(int argc, char *argv[])
     tdisp(v1);
     tdisp(v2);
 
-    // not working
+    // working
     mout << std::endl;
     mout << "Recursive add (add1)" << std::endl;
     v3[0][0] = -1;
@@ -167,7 +167,7 @@ int main(int argc, char *argv[])
   }
 
 
-    {
+  {
     const int N1 = 100000;
     const int n1 = 1000;
     const int N2 = 3;
@@ -197,7 +197,7 @@ int main(int argc, char *argv[])
     tdisp(v1[n1]);
     tdisp(v2[n1]);
 
-    // not working
+    // working
     mout << std::endl;
     mout << "Recursive add (add1)" << std::endl;
     t.start_timer_silent();
@@ -216,8 +216,123 @@ int main(int argc, char *argv[])
     
   }
 
+  {
+    const int N1 = 3;
+    const int n1 = 1;
+    const int N2 = 100000;
 
 
+
+    cr();
+    mout << "" << createStyle(BOLD).apply("Vector<Vector<double>> + Vector<Vector<double>>") << ", N1="<<N1<< ", N2="<<N2<<std::endl;
+    
+    mout << "  " << createStyle(BLUE2+BOLD).apply("matricks::Vector") <<std::endl;
+    Vector<Vector<double>> v1(N1);
+    Vector<Vector<double>> v2(N1);
+    Vector<Vector<double>> v3(N1);
+    Vector<Vector<double>> v4(N1);
+    for (int i = 0; i < N1; i++) {
+      v1[i].resize(N2);
+      v2[i].resize(N2);
+      v3[i].resize(N2);
+      v4[i].resize(N2);
+    }
+    for (int j = 0; j < N2; j++) {
+      v1[0][j] = 1;
+      v1[1][j] = 2;
+      v1[2][j] = 3;
+      v2[0][j] = j;
+      v2[1][j] = j;
+      v2[2][j] = j;
+    }
+    mdisp(v1[0][100],v1[1][100],v1[2][100]);
+    mdisp(v2[0][100],v2[1][100],v2[2][100]);
+
+    
+    // working
+    mout << std::endl;
+    mout << "Recursive add (add1)" << std::endl;
+    t.start_timer_silent();
+    v3.equals1( add1(v1,v2) );
+    t.stop_timer_curt();
+    mdisp(v3[0][100],v3[1][100],v3[2][100]);
+
+    // working
+    mout << std::endl;
+    mout << "Deep add (add2)" << std::endl;
+    t.start_timer_silent();
+    v4.equals2( add2(v1,v2) );
+    t.stop_timer_curt();
+    mdisp(v4[0][100],v4[1][100],v4[2][100]);
+
+    
+  }
+
+
+
+
+  
+  {
+    // fixed length Vectors
+    mout << std::endl;
+    mout << "Fixed Length Vectors" << std::endl;
+    Vector<double,3> v1(5.);
+    tdisp(v1);
+    Vector< Vector<double,3>, 2>  v2 {{1.,2.,3.},{4.,5.,6.}};
+    tdisp(v2);
+  }
+
+
+  {
+    const int N1 = 100000;
+    const int n1 = 1000;
+    const int N2 = 3;
+
+
+    // NOTE: We can't make the our vector fixed because then the executable size will be too big for the loader
+    cr();
+    mout << "" << createStyle(BOLD).apply("Vector<Vector<double,3>> + Vector<Vector<double,3>>") << ", N1="<<N1<< ", N2="<<N2<<std::endl;
+    
+    mout << "  " << createStyle(BLUE2+BOLD).apply("matricks::Vector") <<std::endl;
+    Vector<Vector<double,N2>> v1;
+    Vector<Vector<double,N2>> v2;
+    Vector<Vector<double,N2>> v3;
+    Vector<Vector<double,N2>> v4;
+    v1.resize(N1);
+    v2.resize(N1);
+    v3.resize(N1);
+    v4.resize(N1);
+    for (int i = 0; i < N1; i++) {
+      v1[i][0] = 1;
+      v1[i][1] = 2;
+      v1[i][2] = 3;
+      v2[i][0] = i;
+      v2[i][1] = i;
+      v2[i][2] = i;
+    }
+    tdisp(v1[n1]);
+    tdisp(v2[n1]);
+
+    // working
+    mout << std::endl;
+    mout << "Recursive add (add1)" << std::endl;
+    t.start_timer_silent();
+    v3.equals1( add1(v1,v2) );
+    t.stop_timer_curt();
+    tdisp(v3[n1]);
+
+    // working
+    mout << std::endl;
+    mout << "Deep add (add2)" << std::endl;
+    t.start_timer_silent();
+    v4.equals2( add2(v1,v2) );
+    t.stop_timer_curt();
+    tdisp(v4[n1]);
+
+    
+   }
+
+  
   tfull.stop_timer_silent();
   cr();
   printf("TOTAL TIME ELAPSED (including text display) = %f sec\n",tfull.cputime());

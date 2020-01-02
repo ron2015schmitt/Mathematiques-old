cb925dfeffe2a197f6c0e93c9efc7bbe5854adef
ron2015schmitt <Ron.Schmitt@caa.columbia.edu>
2020-01-02 18:56:29 -0500

    doc TOC

--- doc/vdebug.cpp
+++ doc/vdebug.cpp
@@ -23,11 +23,52 @@ int main()
   
   mathq_toc();
   cr();cr();
-  mdtitle("Vector Debugging (`MEBUG=1`)");
+  mdtitle("Debugging");
   mathq_preamble();
 
+
+
+  text("The Mathématiques library provides extensive debugging operation, turned on using the flag `MATHQ_DEBUG`");
+
+  text("In this mode extensive error checking is performed to notify the programmer of");
+
+  text(" - out of bounds access for vectors or matrices (e.g. x[10] where x has size less than 9");
+  text(" - adding/subtracting/multiplying/dividing vector or matrices of unequal size");
+  text(" - invalid dot products of vector and/or matrices ");
+  text(" - other assorted errors and warnings");
+
+
+  header2("SETTING `MATHQ_DEBUG` MODE");
+
+  text("There are two methods to accomplish this.");
+
+  text("1. Use the provided sample makefile and specify `MATHQ_DEBUG=1` on the command line");
+
+  text("```make clean MATHQ_DEBUG=1 myprogram```");
+
+  text("The target `clean` is not needed, but is a recomended practice.");
+
+
+  text("1. Add a `#define MATHQ_DEBUG 1` statement BEFORE each `mathq.h` include statement");
+
+
+  codestart("C++");
+
+  codemulti("#define MATHQ_DEBUG 1");
+  codemulti("#include \"mathq.h\"");
+  codeend();
+
+  header2("DEBUG LEVELS");
+
+  text(" to be written...");
+
+  header2("RECOMMENDATIONS");
+
+  text("* While developing your code, use `MATHQ_DEBUG` mode.  When you are convinced that it is operating without errors or warnings, run normally.");
+  
+  text("* Compile a `MATHQ_DEBUG` and a quick version of your code, giving the executables different names. This way if a segmentation fault occurs, or you otherwise suspect an error, you can quickly check the problem under careful mode.");
+  
   mathq_toc();
-
-
+  
   return 0;
 }
